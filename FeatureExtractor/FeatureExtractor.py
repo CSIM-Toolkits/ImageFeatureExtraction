15fba3467e2994bb55a07621823bfe7c37c233f7
Antonio Carlos da Silva Senra Filho <acsenrafilho@gmail.com>
2017-10-03 15:58:05 -0300

    ENH: ZScore mapping Pyhton and CLI is operational
    At the moment, the ZScore mapping is running from both Python and CLI modules.

---
+++ FeatureExtractor/FeatureExtractor.py
@@ -0,0 +1,325 @@
+import os
+import sys
+import platform
+import unittest
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+import logging
+
+#
+# FeatureExtractor
+#
+
+class FeatureExtractor(ScriptedLoadableModule):
+  """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
+    self.parent.title = "Feature Extractor"
+    self.parent.categories = ["Examples"]
+    self.parent.dependencies = []
+    self.parent.contributors = ["John Doe (AnyWare Corp.)"] # replace with "Firstname Lastname (Organization)"
+    self.parent.helpText = """
+This is an example of scripted loadable module bundled in an extension.
+It performs a simple thresholding on the input volume and optionally captures a screenshot.
+"""
+    self.parent.helpText += self.getDefaultModuleDocumentationLink()
+    self.parent.acknowledgementText = """
+This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+""" # replace with organization, grant and thanks.
+
+#
+# FeatureExtractorWidget
+#
+
+class FeatureExtractorWidget(ScriptedLoadableModuleWidget):
+  """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setup(self):
+    ScriptedLoadableModuleWidget.setup(self)
+
+    # Instantiate and connect widgets ...
+
+    #
+    # Parameters Area
+    #
+    parametersCollapsibleButton = ctk.ctkCollapsibleButton()
+    parametersCollapsibleButton.text = "Z-Score Mapping"
+    # parametersCollapsibleButton.collapsed = True
+    self.layout.addWidget(parametersCollapsibleButton)
+
+    # Layout within the dummy collapsible button
+    parametersZScoreLayout = qt.QFormLayout(parametersCollapsibleButton)
+
+    #
+    # input volume selector
+    #
+    self.inputSelector = slicer.qMRMLNodeComboBox()
+    self.inputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    self.inputSelector.selectNodeUponCreation = True
+    self.inputSelector.addEnabled = False
+    self.inputSelector.removeEnabled = False
+    self.inputSelector.noneEnabled = False
+    self.inputSelector.showHidden = False
+    self.inputSelector.showChildNodeTypes = False
+    self.inputSelector.setMRMLScene( slicer.mrmlScene )
+    self.inputSelector.setToolTip( "Pick the input to the algorithm." )
+    parametersZScoreLayout.addRow("Input Volume: ", self.inputSelector)
+
+
+    #
+    # output volume selector
+    #
+    self.outputSelector = slicer.qMRMLNodeComboBox()
+    self.outputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    self.outputSelector.selectNodeUponCreation = True
+    self.outputSelector.addEnabled = True
+    self.outputSelector.removeEnabled = True
+    self.outputSelector.noneEnabled = False
+    self.outputSelector.showHidden = False
+    self.outputSelector.showChildNodeTypes = False
+    self.outputSelector.setMRMLScene( slicer.mrmlScene )
+    self.outputSelector.setToolTip( "Pick the output to the algorithm." )
+    parametersZScoreLayout.addRow("Output Volume: ", self.outputSelector)
+
+    #
+    # region mask selector
+    #
+    self.regionMaskSelector = slicer.qMRMLNodeComboBox()
+    self.regionMaskSelector.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
+    self.regionMaskSelector.selectNodeUponCreation = True
+    self.regionMaskSelector.addEnabled = False
+    self.regionMaskSelector.removeEnabled = False
+    self.regionMaskSelector.noneEnabled = True
+    self.regionMaskSelector.showHidden = False
+    self.regionMaskSelector.showChildNodeTypes = False
+    self.regionMaskSelector.setMRMLScene( slicer.mrmlScene )
+    self.regionMaskSelector.setToolTip( "Pick ..." )
+    parametersZScoreLayout.addRow("Region Mask: ", self.regionMaskSelector)
+
+    #
+    # Image Modality
+    #
+    self.setImageModalityComboBoxWidget = ctk.ctkComboBox()
+    self.setImageModalityComboBoxWidget.addItem("T1")
+    self.setImageModalityComboBoxWidget.addItem("T2")
+    self.setImageModalityComboBoxWidget.addItem("PD")
+    self.setImageModalityComboBoxWidget.addItem("DTI-FA")
+    self.setImageModalityComboBoxWidget.addItem("DTI-ADC")
+    self.setImageModalityComboBoxWidget.addItem("DTI-RA")
+    self.setImageModalityComboBoxWidget.addItem("DTI-RD")
+    self.setImageModalityComboBoxWidget.setToolTip(
+      "....")
+    parametersZScoreLayout.addRow("Image Modality ", self.setImageModalityComboBoxWidget)
+
+    #
+    # Image Resolution
+    #
+    self.setImageResolutionComboBoxWidget = ctk.ctkComboBox()
+    self.setImageResolutionComboBoxWidget.addItem("1mm")
+    self.setImageResolutionComboBoxWidget.addItem("2mm")
+    self.setImageResolutionComboBoxWidget.setToolTip(
+      "....")
+    parametersZScoreLayout.addRow("Image Resolution ", self.setImageResolutionComboBoxWidget)
+
+    #
+    # Apply Histogram Matching?
+    #
+    self.setDoHistogramMatchingBooleanWidget = ctk.ctkCheckBox()
+    self.setDoHistogramMatchingBooleanWidget.setChecked(False)
+    self.setDoHistogramMatchingBooleanWidget.setToolTip(
+      "....")
+    parametersZScoreLayout.addRow("Apply Histogram Matching",
+                                      self.setDoHistogramMatchingBooleanWidget)
+
+    #
+    # Apply Button
+    #
+    self.applyButton = qt.QPushButton("Apply")
+    self.applyButton.toolTip = "Run the algorithm."
+    self.applyButton.enabled = False
+    parametersZScoreLayout.addRow(self.applyButton)
+
+    # connections
+    self.applyButton.connect('clicked(bool)', self.onApplyButton)
+    self.inputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+    self.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+
+    # Add vertical spacer
+    self.layout.addStretch(1)
+
+    # Refresh Apply button state
+    self.onSelect()
+
+  def cleanup(self):
+    pass
+
+  def onSelect(self):
+    self.applyButton.enabled = self.inputSelector.currentNode() and self.outputSelector.currentNode()
+
+  def onApplyButton(self):
+    logic = FeatureExtractorLogic()
+    modality=self.setImageModalityComboBoxWidget.currentText
+    resolution=self.setImageResolutionComboBoxWidget.currentText
+    doHistMatch=self.setDoHistogramMatchingBooleanWidget.isChecked()
+    logic.run(self.inputSelector.currentNode(), self.outputSelector.currentNode(),self.regionMaskSelector.currentNode(),modality,resolution,doHistMatch)
+
+#
+# FeatureExtractorLogic
+#
+
+class FeatureExtractorLogic(ScriptedLoadableModuleLogic):
+  """This class should implement all the actual
+  computation done by your module.  The interface
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget.
+  Uses ScriptedLoadableModuleLogic base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def hasImageData(self,volumeNode):
+    """This is an example logic method that
+    returns true if the passed in volume
+    node has valid image data
+    """
+    if not volumeNode:
+      logging.debug('hasImageData failed: no volume node')
+      return False
+    if volumeNode.GetImageData() is None:
+      logging.debug('hasImageData failed: no image data in volume node')
+      return False
+    return True
+
+  def isValidInputOutputData(self, inputVolumeNode, outputVolumeNode):
+    """Validates if the output is not the same as input
+    """
+    if not inputVolumeNode:
+      logging.debug('isValidInputOutputData failed: no input volume node defined')
+      return False
+    if not outputVolumeNode:
+      logging.debug('isValidInputOutputData failed: no output volume node defined')
+      return False
+    if inputVolumeNode.GetID()==outputVolumeNode.GetID():
+      logging.debug('isValidInputOutputData failed: input and output volume is the same. Create a new volume for output to avoid this error.')
+      return False
+    return True
+
+  def run(self, inputVolume, outputVolume, regionMask, modality, resolution, doHistMatch):
+    """
+    Run the actual algorithm
+    """
+
+    if not self.isValidInputOutputData(inputVolume, outputVolume):
+      slicer.util.errorDisplay('Input volume is the same as output volume. Choose a different output volume.')
+      return False
+
+    logging.info('Processing started')
+    slicer.util.showStatusMessage("Processing started")
+
+    # Get the path to BrainTemplates files
+    path2files = os.path.dirname(slicer.modules.featureextractor.path)
+
+    templateMeanFileName=""
+    templateSTDFileName=""
+    if "DTI" not in modality:
+      templateMeanFileName="IXI-ICBM-"+modality+"mean-"+resolution+".nii.gz"
+      templateSTDFileName = "IXI-ICBM-" + modality + "std-" + resolution + ".nii.gz"
+    else:
+      templateMeanFileName="USP-ICBM-"+modality+"mean-131-"+resolution+".nii.gz"
+      templateSTDFileName = "USP-ICBM-" + modality + "std-131-" + resolution + ".nii.gz"
+
+    if platform.system() is "Windows":
+      (read, MNITemplateMeanNode) = slicer.util.loadVolume(
+        path2files + '\\Resources\\BrainTemplates\\'+templateMeanFileName,
+        {}, True)
+      (read, MNITemplateSTDNode) = slicer.util.loadVolume(
+        path2files + '\\Resources\\BrainTemplates\\' + templateSTDFileName,
+        {}, True)
+    else:
+      (read, MNITemplateMeanNode) = slicer.util.loadVolume(
+      path2files + '/Resources/BrainTemplates/'+templateMeanFileName, {}, True)
+      (read, MNITemplateSTDNode) = slicer.util.loadVolume(
+        path2files + '/Resources/BrainTemplates/' + templateSTDFileName, {}, True)
+
+    # Run Z-Score Mapping
+    slicer.util.showStatusMessage("Calculating Z-Score map")
+    params={}
+    params['inputVolume']=inputVolume.GetID()
+    params['inputTemplateMean']=MNITemplateMeanNode.GetID()
+    params['inputTemplateStd']=MNITemplateSTDNode.GetID()
+    params['outputVolume']=outputVolume.GetID()
+    if regionMask is not None:
+      params['regionMask']=regionMask.GetID()
+    params['doHistogramMatching']=doHistMatch
+
+    slicer.cli.run(slicer.modules.zscoremapping, None, params, wait_for_completion=True)
+
+    # Removing unnecessary nodes
+    slicer.mrmlScene.RemoveNode(MNITemplateMeanNode)
+    slicer.mrmlScene.RemoveNode(MNITemplateSTDNode)
+
+    logging.info('Processing completed')
+    slicer.util.showStatusMessage("Processing completed")
+
+    return True
+
+
+class FeatureExtractorTest(ScriptedLoadableModuleTest):
+  """
+  This is the test case for your scripted module.
+  Uses ScriptedLoadableModuleTest base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear(0)
+
+  def runTest(self):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    self.test_FeatureExtractor1()
+
+  def test_FeatureExtractor1(self):
+    """ Ideally you should have several levels of tests.  At the lowest level
+    tests should exercise the functionality of the logic with different inputs
+    (both valid and invalid).  At higher levels your tests should emulate the
+    way the user would interact with your code and confirm that it still works
+    the way you intended.
+    One of the most important features of the tests is that it should alert other
+    developers when their changes will have an impact on the behavior of your
+    module.  For example, if a developer removes a feature that you depend on,
+    your test should break so they know that the feature is needed.
+    """
+
+    self.delayDisplay("Starting the test")
+    #
+    # first, get some data
+    #
+    import urllib
+    downloads = (
+        ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
+        )
+
+    for url,name,loader in downloads:
+      filePath = slicer.app.temporaryPath + '/' + name
+      if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
+        logging.info('Requesting download %s from %s...\n' % (name, url))
+        urllib.urlretrieve(url, filePath)
+      if loader:
+        logging.info('Loading %s...' % (name,))
+        loader(filePath)
+    self.delayDisplay('Finished with download and loading')
+
+    volumeNode = slicer.util.getNode(pattern="FA")
+    logic = FeatureExtractorLogic()
+    self.assertIsNotNone( logic.hasImageData(volumeNode) )
+    self.delayDisplay('Test passed!')
--- ZScoreMapping/ZScoreMapping.cxx
+++ ZScoreMapping/ZScoreMapping.cxx
@@ -1,9 +1,8 @@
 #include "itkImageFileWriter.h"
 
-#include "itkSmoothingRecursiveGaussianImageFilter.h"
 #include "itkImageRegionIterator.h"
 #include "itkHistogramMatchingImageFilter.h"
-
+#include <math.h>
 #include "itkPluginUtilities.h"
 
 #include "ZScoreMappingCLP.h"
@@ -19,63 +18,63 @@ namespace
 template <typename TPixel>
 int DoIt( int argc, char * argv[], TPixel )
 {
-  PARSE_ARGS;
+    PARSE_ARGS;
 
-  typedef TPixel        InputPixelType;
-  typedef float         OutputPixelType;
-  typedef unsigned char LabelPixelType;
+    typedef TPixel        InputPixelType;
+    typedef float         OutputPixelType;
+    typedef unsigned char LabelPixelType;
 
-  const unsigned int Dimension = 3;
+    const unsigned int Dimension = 3;
 
-  typedef itk::Image<InputPixelType,  Dimension> InputImageType;
-  typedef itk::Image<LabelPixelType,  Dimension> LabelImageType;
-  typedef itk::Image<OutputPixelType, Dimension> OutputImageType;
+    typedef itk::Image<InputPixelType,  Dimension> InputImageType;
+    typedef itk::Image<LabelPixelType,  Dimension> LabelImageType;
+    typedef itk::Image<OutputPixelType, Dimension> OutputImageType;
 
-  typedef itk::ImageFileReader<InputImageType>  ReaderType;
-  typedef itk::ImageFileReader<LabelImageType>  LabelReaderType;
+    typedef itk::ImageFileReader<InputImageType>  ReaderType;
+    typedef itk::ImageFileReader<LabelImageType>  LabelReaderType;
 
-  typename ReaderType::Pointer readerInput = ReaderType::New();
-  typename ReaderType::Pointer readerTemplateMean = ReaderType::New();
-  typename ReaderType::Pointer readerTemplateSTD = ReaderType::New();
-  typename LabelReaderType::Pointer readerRegionMask = LabelReaderType::New();
+    typename ReaderType::Pointer readerInput = ReaderType::New();
+    typename ReaderType::Pointer readerTemplateMean = ReaderType::New();
+    typename ReaderType::Pointer readerTemplateSTD = ReaderType::New();
+    typename LabelReaderType::Pointer readerRegionMask = LabelReaderType::New();
 
-  readerInput->SetFileName( inputVolume.c_str() );
-  readerTemplateMean->SetFileName( inputTemplateMean.c_str() );
-  readerTemplateSTD->SetFileName( inputTemplateStd.c_str() );
-  readerInput->Update();
-  readerTemplateMean->Update();
-  readerTemplateSTD->Update();
+    readerInput->SetFileName( inputVolume.c_str() );
+    readerTemplateMean->SetFileName( inputTemplateMean.c_str() );
+    readerTemplateSTD->SetFileName( inputTemplateStd.c_str() );
+    readerInput->Update();
+    readerTemplateMean->Update();
+    readerTemplateSTD->Update();
 
-  //Apply histogram matching
-  typedef itk::HistogramMatchingImageFilter<InputImageType,InputImageType>        HistogramMatchingType;
-typename HistogramMatchingType::Pointer hMatch = HistogramMatchingType::New();
-  if (doHistogramMatching) {
-      std::cout<<"Performing histogram matching"<<std::endl;
+    //Apply histogram matching
+    typedef itk::HistogramMatchingImageFilter<InputImageType,InputImageType>        HistogramMatchingType;
+    typename HistogramMatchingType::Pointer hMatch = HistogramMatchingType::New();
+    if (doHistogramMatching) {
+        std::cout<<"Performing histogram matching"<<std::endl;
 
-      hMatch->SetReferenceImage( readerTemplateMean->GetOutput() );
-      hMatch->SetInput( readerInput->GetOutput() );
-      hMatch->SetNumberOfHistogramLevels( 255 );
-      hMatch->SetNumberOfMatchPoints( 64 );
-      hMatch->Update();
-  }
+        hMatch->SetReferenceImage( readerTemplateMean->GetOutput() );
+        hMatch->SetInput( readerInput->GetOutput() );
+        hMatch->SetNumberOfHistogramLevels( 255 );
+        hMatch->SetNumberOfMatchPoints( 64 );
+        hMatch->Update();
+    }
 
-  //Calculate Z-Score map
-  typename OutputImageType::Pointer zScoreMap = OutputImageType::New();
-  zScoreMap->CopyInformation(readerInput->GetOutput());
-  zScoreMap->SetRegions(readerInput->GetOutput()->GetRequestedRegion());
-  zScoreMap->Allocate();
-  zScoreMap->FillBuffer(0);
+    //Calculate Z-Score map
+    typename OutputImageType::Pointer zScoreMap = OutputImageType::New();
+    zScoreMap->CopyInformation(readerInput->GetOutput());
+    zScoreMap->SetRegions(readerInput->GetOutput()->GetRequestedRegion());
+    zScoreMap->Allocate();
+    zScoreMap->FillBuffer(0);
 
-  typedef itk::ImageRegionIterator<InputImageType>      RegionIteratorType;
-  typedef itk::ImageRegionIterator<OutputImageType>      OutputRegionIteratorType;
+    typedef itk::ImageRegionIterator<InputImageType>      RegionIteratorType;
+    typedef itk::ImageRegionIterator<OutputImageType>      OutputRegionIteratorType;
 
-  RegionIteratorType imgIt(doHistogramMatching?hMatch->GetOutput():readerInput->GetOutput(),
-                           doHistogramMatching?hMatch->GetOutput()->GetRequestedRegion():readerInput->GetOutput()->GetRequestedRegion());
-  RegionIteratorType tempMeanIt(readerTemplateMean->GetOutput(), readerTemplateMean->GetOutput()->GetRequestedRegion());
-  RegionIteratorType tempStdIt(readerTemplateSTD->GetOutput(), readerTemplateSTD->GetOutput()->GetRequestedRegion());
-  OutputRegionIteratorType zScoreIt(zScoreMap, zScoreMap->GetRequestedRegion());
+    RegionIteratorType imgIt(doHistogramMatching?hMatch->GetOutput():readerInput->GetOutput(),
+                             doHistogramMatching?hMatch->GetOutput()->GetRequestedRegion():readerInput->GetOutput()->GetRequestedRegion());
+    RegionIteratorType tempMeanIt(readerTemplateMean->GetOutput(), readerTemplateMean->GetOutput()->GetRequestedRegion());
+    RegionIteratorType tempStdIt(readerTemplateSTD->GetOutput(), readerTemplateSTD->GetOutput()->GetRequestedRegion());
+    OutputRegionIteratorType zScoreIt(zScoreMap, zScoreMap->GetRequestedRegion());
 
-  if (regionMask == "") {
+    if (regionMask == "") {
         std::cout<<"Calculating Z-Score mapping - full brain coverage"<<std::endl;
 
         imgIt.GoToBegin();
@@ -87,115 +86,122 @@ typename HistogramMatchingType::Pointer hMatch = HistogramMatchingType::New();
             if (tempMeanIt.Get()>static_cast<InputPixelType>(0)) {
                 temp_zscore = (static_cast<OutputPixelType>(imgIt.Get()) - static_cast<OutputPixelType>(tempMeanIt.Get()))
                         / static_cast<OutputPixelType>(tempStdIt.Get());
-                zScoreIt.Set(temp_zscore);
+
+                //Preventing to add outlier in the z-score map
+                if (temp_zscore> -10.0 && temp_zscore<10.0) {
+                    zScoreIt.Set(temp_zscore);
+                }
             }
             ++imgIt;
             ++tempMeanIt;
             ++tempStdIt;
             ++zScoreIt;
         }
-  }else{
-      std::cout<<"Calculating Z-Score mapping - region defined in label map"<<std::endl;
+    }else{
+        std::cout<<"Calculating Z-Score mapping - region defined in label map"<<std::endl;
 
-      readerRegionMask->SetFileName( regionMask.c_str() );
-      readerRegionMask->Update();
+        readerRegionMask->SetFileName( regionMask.c_str() );
+        readerRegionMask->Update();
 
-      typedef itk::ImageRegionIterator<LabelImageType>      LabelIteratorType;
-      LabelIteratorType regionIt(readerRegionMask->GetOutput(), readerRegionMask->GetOutput()->GetRequestedRegion());
+        typedef itk::ImageRegionIterator<LabelImageType>      LabelIteratorType;
+        LabelIteratorType regionIt(readerRegionMask->GetOutput(), readerRegionMask->GetOutput()->GetRequestedRegion());
 
-      imgIt.GoToBegin();
-      tempMeanIt.GoToBegin();
-      tempStdIt.GoToBegin();
-      regionIt.GoToBegin();
-      zScoreIt.GoToBegin();
-      float temp_zscore=0.0;
-      while(!imgIt.IsAtEnd()){
-          if (regionIt.Get()>static_cast<LabelPixelType>(0)) {
-              temp_zscore = (static_cast<OutputPixelType>(imgIt.Get()) - static_cast<OutputPixelType>(tempMeanIt.Get()))
-                      / static_cast<OutputPixelType>(tempStdIt.Get());
-              zScoreIt.Set(temp_zscore);
-          }
-          ++imgIt;
-          ++tempMeanIt;
-          ++tempStdIt;
-          ++regionIt;
-          ++zScoreIt;
-      }
-  }
+        imgIt.GoToBegin();
+        tempMeanIt.GoToBegin();
+        tempStdIt.GoToBegin();
+        regionIt.GoToBegin();
+        zScoreIt.GoToBegin();
+        float temp_zscore=0.0;
+        while(!imgIt.IsAtEnd()){
+            if (regionIt.Get()>static_cast<LabelPixelType>(0)) {
+                temp_zscore = (static_cast<OutputPixelType>(imgIt.Get()) - static_cast<OutputPixelType>(tempMeanIt.Get()))
+                        / static_cast<OutputPixelType>(tempStdIt.Get());
+                //Preventing to add outlier in the z-score map
+                if (temp_zscore > -10.0 && temp_zscore < 10.0) {
+                    zScoreIt.Set(temp_zscore);
+                }
+            }
+            ++imgIt;
+            ++tempMeanIt;
+            ++tempStdIt;
+            ++regionIt;
+            ++zScoreIt;
+        }
+    }
 
 
-  typedef itk::ImageFileWriter<OutputImageType> WriterType;
-  typename WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName( outputVolume.c_str() );
-  writer->SetInput( zScoreMap );
-  writer->SetUseCompression(1);
-  writer->Update();
+    typedef itk::ImageFileWriter<OutputImageType> WriterType;
+    typename WriterType::Pointer writer = WriterType::New();
+    writer->SetFileName( outputVolume.c_str() );
+    writer->SetInput( zScoreMap );
+    writer->SetUseCompression(1);
+    writer->Update();
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 } // end of anonymous namespace
 
 int main( int argc, char * argv[] )
 {
-  PARSE_ARGS;
+    PARSE_ARGS;
 
-  itk::ImageIOBase::IOPixelType     pixelType;
-  itk::ImageIOBase::IOComponentType componentType;
+    itk::ImageIOBase::IOPixelType     pixelType;
+    itk::ImageIOBase::IOComponentType componentType;
 
-  try
+    try
     {
-    itk::GetImageType(inputVolume, pixelType, componentType);
+        itk::GetImageType(inputVolume, pixelType, componentType);
 
-    // This filter handles all types on input, but only produces
-    // signed types
-    switch( componentType )
-      {
-      case itk::ImageIOBase::UCHAR:
-        return DoIt( argc, argv, static_cast<unsigned char>(0) );
-        break;
-      case itk::ImageIOBase::CHAR:
-        return DoIt( argc, argv, static_cast<signed char>(0) );
-        break;
-      case itk::ImageIOBase::USHORT:
-        return DoIt( argc, argv, static_cast<unsigned short>(0) );
-        break;
-      case itk::ImageIOBase::SHORT:
-        return DoIt( argc, argv, static_cast<short>(0) );
-        break;
-      case itk::ImageIOBase::UINT:
-        return DoIt( argc, argv, static_cast<unsigned int>(0) );
-        break;
-      case itk::ImageIOBase::INT:
-        return DoIt( argc, argv, static_cast<int>(0) );
-        break;
-      case itk::ImageIOBase::ULONG:
-        return DoIt( argc, argv, static_cast<unsigned long>(0) );
-        break;
-      case itk::ImageIOBase::LONG:
-        return DoIt( argc, argv, static_cast<long>(0) );
-        break;
-      case itk::ImageIOBase::FLOAT:
-        return DoIt( argc, argv, static_cast<float>(0) );
-        break;
-      case itk::ImageIOBase::DOUBLE:
-        return DoIt( argc, argv, static_cast<double>(0) );
-        break;
-      case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
-      default:
-        std::cerr << "Unknown input image pixel component type: ";
-        std::cerr << itk::ImageIOBase::GetComponentTypeAsString( componentType );
-        std::cerr << std::endl;
+        // This filter handles all types on input, but only produces
+        // signed types
+        switch( componentType )
+        {
+        case itk::ImageIOBase::UCHAR:
+            return DoIt( argc, argv, static_cast<unsigned char>(0) );
+            break;
+        case itk::ImageIOBase::CHAR:
+            return DoIt( argc, argv, static_cast<signed char>(0) );
+            break;
+        case itk::ImageIOBase::USHORT:
+            return DoIt( argc, argv, static_cast<unsigned short>(0) );
+            break;
+        case itk::ImageIOBase::SHORT:
+            return DoIt( argc, argv, static_cast<short>(0) );
+            break;
+        case itk::ImageIOBase::UINT:
+            return DoIt( argc, argv, static_cast<unsigned int>(0) );
+            break;
+        case itk::ImageIOBase::INT:
+            return DoIt( argc, argv, static_cast<int>(0) );
+            break;
+        case itk::ImageIOBase::ULONG:
+            return DoIt( argc, argv, static_cast<unsigned long>(0) );
+            break;
+        case itk::ImageIOBase::LONG:
+            return DoIt( argc, argv, static_cast<long>(0) );
+            break;
+        case itk::ImageIOBase::FLOAT:
+            return DoIt( argc, argv, static_cast<float>(0) );
+            break;
+        case itk::ImageIOBase::DOUBLE:
+            return DoIt( argc, argv, static_cast<double>(0) );
+            break;
+        case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
+        default:
+            std::cerr << "Unknown input image pixel component type: ";
+            std::cerr << itk::ImageIOBase::GetComponentTypeAsString( componentType );
+            std::cerr << std::endl;
+            return EXIT_FAILURE;
+            break;
+        }
+    }
+
+    catch( itk::ExceptionObject & excep )
+    {
+        std::cerr << argv[0] << ": exception caught !" << std::endl;
+        std::cerr << excep << std::endl;
         return EXIT_FAILURE;
-        break;
-      }
     }
-
-  catch( itk::ExceptionObject & excep )
-    {
-    std::cerr << argv[0] << ": exception caught !" << std::endl;
-    std::cerr << excep << std::endl;
-    return EXIT_FAILURE;
-    }
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
